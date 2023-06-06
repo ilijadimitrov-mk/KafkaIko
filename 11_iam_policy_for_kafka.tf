9e0e7f1b273f3649d69980d1fdbfa9073c88be21
ilijadimitrov-rldatix <ilija.dimitrov@rldatix.com>
2023-06-06 09:20:39 +0200

    Data settings

--- 11_iam_policy_for_kafka.tf
+++ 11_iam_policy_for_kafka.tf
@@ -16,7 +16,8 @@ resource "aws_iam_policy" "IAMSysOpsMSKPolicy" {
                 "kafka-cluster:DescribeCluster"
             ],
             "Resource": [
-                "arn:aws:kafka:us-east-2:386743670652:cluster/*/*"
+                # "arn:aws:kafka:us-east-2:386743670652:cluster/*/*"
+                "arn:aws:kafka:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:cluster/*/*"
             ]
         },
         {
@@ -27,7 +28,8 @@ resource "aws_iam_policy" "IAMSysOpsMSKPolicy" {
                 "kafka-cluster:ReadData"
             ],
             "Resource": [
-                "arn:aws:kafka:us-east-2:386743670652:topic/*/*"
+                #"arn:aws:kafka:us-east-2:386743670652:topic/*/*"
+                "arn:aws:kafka:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:topic/*/*"
             ]
         },
         {
@@ -37,7 +39,8 @@ resource "aws_iam_policy" "IAMSysOpsMSKPolicy" {
                 "kafka-cluster:DescribeGroup"
             ],
             "Resource": [
-                "arn:aws:kafka:us-east-2:386743670652:group/*/*"
+                #"arn:aws:kafka:us-east-2:386743670652:group/*/*"
+                "arn:aws:kafka:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:group/*/*"
             ]
         }
     ]
